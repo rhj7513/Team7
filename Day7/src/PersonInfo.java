2f52eb2585d8e1ccd07c86d4e66e30d6cc244537
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2022-06-29 10:42:42 +0900

    PersonInfo
    class

---
+++ Day7/src/PersonInfo.java
@@ -0,0 +1,75 @@
+
+public class PersonInfo {
+	//필드
+	private String name; //수정
+	private int birthday;
+	private int call; //수정
+	private String address; //수정
+	private int accountNum; //수정
+	private String password; //수정
+
+	//생성자
+	public PersonInfo() {//빈
+		
+	}
+	
+	public PersonInfo(String n, int b, int c, String ad, int an, String pw) {//전체
+		name = n;
+		birthday = b;
+		call = c;
+		address = ad;
+		accountNum = an;
+		password = pw;
+	}
+	
+	
+	
+	//get추가함
+	public String getName() {
+		return name;
+	}
+	public int getBirthday() {
+		return birthday;
+	}
+	public int getCall() {
+		return call;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public int getAccountNum() {
+		return accountNum;
+	}
+	public String getPassword() {
+		return password;
+	}
+	
+	
+	
+	//set추가함
+	public void setName(String n) {
+		this.name=n;
+	}
+	public void setBirthday(int b) {
+		this.birthday = b;
+	}
+	public void setCall(int c) {
+		this.call = c;
+	}
+	public void setAddress(String ad) {
+		this.address = ad;
+	}
+	public void setAccountNum(int an) {
+		this.accountNum = an;
+	}
+	public void setPassword(String pw) {
+		this.password = pw;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+  
