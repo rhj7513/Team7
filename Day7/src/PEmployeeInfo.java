82bfb0911a83e8dd2fc05c66c415f2418fd41dd2
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2022-06-29 11:20:49 +0900

    PEmployeeInfo
    비정규직 사원정보 추가

--- Day7/src/PEmployeeInfo.java
+++ Day7/src/PEmployeeInfo.java
@@ -1,6 +1,48 @@
 
 public class PEmployeeInfo {
+	private PersonInfo[]person;
+	//private SalaryOp[]salaryop; -> SalaryOp class생성 후 추가
 
+	//필드
+	private String employeeNum;
+	private String department;
+	private int salary;
+	
+	//생성자
+	public PEmployeeInfo() {//빈
+		
+	}
+	
+	public PEmployeeInfo(String en, String d, int s) { //전체
+		employeeNum = en;
+		department = d;
+		salary = s;
+	}
+	
+	//get추가함
+	public String getEmployeeNum() {
+		return employeeNum;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	
+	
+	//set추가함
+	public void setEmployeeNum(String en) {
+		this.employeeNum = en;
+	}
+	public void setDepartment(String d) {
+		this.department = d;
+	}
+	public void setSalary(int s) {
+		this.salary = s;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
