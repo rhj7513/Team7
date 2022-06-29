10d9d213a0aba16e445b6ea82d87056e3dddb3b7
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2022-06-29 11:01:31 +0900

    FEmployeeInfo
    정규직 사원정보 추가

---
+++ FEmployeeInfo.java
@@ -0,0 +1,65 @@
+
+public class FEmployeeInfo {
+	private PersonInfo[]person;
+	//private SalaryOp[]salaryop; -> SalaryOp class생성 후 추가
+	//필드
+	private String employeeNum;
+	private String position;
+	private String department;
+	private int year;
+	private int salary;
+	
+	//생성자
+	public FEmployeeInfo() {//빈
+		
+	}
+	
+	public FEmployeeInfo(String en, String p, String d, int y, int s) { //전체
+		employeeNum = en;
+		position = p;
+		department = d;
+		year = y;
+		salary = s;
+	}
+
+	//get추가함
+	public String getEmployeeNum() {
+		return employeeNum;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public int getYear() {
+		return year;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	
+	//set추가함
+	public void setEmployeeNum(String en) {
+		this.employeeNum = en;
+	}
+	public void setPosition(String p) {
+		this.position = p;
+	}
+	public void setDepartment(String d) {
+		this.department = d;
+	}
+	public void setYear(int y) {
+		this.year = y;
+	}
+	public void setSalary(int s) {
+		this.salary = s;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
